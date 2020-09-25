5c3e03097da6d624f549e7c306fc139fd90a2f97
danilozte <danilozte@hotmail.com>
2020-09-25 09:03:32 -0500

    Nuevas lineas de codigo y  ejerci

---
+++ HELLO PYTHON .py
@@ -0,0 +1,34 @@
+# strings
+print("Hello world")
+
+#Numbers
+print(30)
+
+#Float 
+print(30.22)
+
+# Boolean tipos de estados palabras recervas 
+False 
+True
+
+# list de enteros o textos que pueden cambiar
+["bye", 10, "Adios"]
+[10,  20, 30, 55]
+
+#Tuples es una lista que no puedo cambiar , "inmutable"
+
+(10, 20 , 30,)
+
+#Dictionaries agrupar datos que pertenecen a una misma entidad.
+
+
+print(type({
+   
+    "nombre de la persona" : "Ryan",
+    "last name"            :  "Ray" ,
+    "nickname"             :  "Fast"
+}))
+
+print(type(None))
+
+# Variables
---
+++ VARIABLES.py
@@ -0,0 +1,31 @@
+# variables
+import time
+pedro = 10
+juan = 20
+
+# if time.time() < 18:
+#     print ("ES VALIDO")
+# else:  print ("ES invalido")
+# print (20 < juan)
+
+# # para valores que no cambian SE UTILIZAN LAS VARIABLES EN MAYUSCULAS
+
+
+# PI = 3,1416
+
+# # CUANDO ES UN LENGUAJE DINAMICO, puedes usar dos valores distintos para una misma constante
+
+# book_name = "I phone"
+# book_name = 123456
+hour = 0
+while True:
+    if(hour >=18):
+        print("Ya son las 6")
+     else:
+        print("Aun no son las 6")
+    hour = hour+1
+
+#weg 
+
+
+
